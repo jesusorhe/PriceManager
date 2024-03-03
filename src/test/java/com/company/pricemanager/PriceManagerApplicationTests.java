ef24e5915788dd5fadd5bc497bfc4bb363f1f4ab
Jesus <jesusorhe@gmail.com>
2024-03-03 13:07:12 +0100

    Generate project and add database connection and data

---
+++ src/test/java/com/company/pricemanager/PriceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.pricemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
